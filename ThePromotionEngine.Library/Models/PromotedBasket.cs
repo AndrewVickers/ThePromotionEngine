4c79327fefdf1170a24ec63ec272dd074ccad509
Andrew Vickers <dev@andrewvickers.net>
2021-08-09 15:15:24 +0100

    Trying to match up a basket and promotions

---
+++ ThePromotionEngine.Library/Models/PromotedBasket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePromotionEngine.Library.Models
+{
+    public class PromotedBasket
+    {
+        public struct promotedProduct
+        {
+            public string Name;
+            public Decimal Modifier;
+            public int Price;
+        }
+
+        public int Id { get; set; }
+        public IList<promotedProduct> ProductList { get; set; }
+        public decimal Total;
+
+    }
+}
---
+++ ThePromotionEngine.UnitTests/BasketPromotionTasksShould.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThePromotionEngine.Library.Models;
+using ThePromotionEngine.Library.Tasks;
+using Xunit;
+
+namespace ThePromotionEngine.UnitTests
+{
+    public class BasketPromotionTasksShould
+    {
+        private BasketPromotionTasks _sut;
+        private Promotion[] _promotionList;
+
+        public BasketPromotionTasksShould()
+        {
+            _sut = new BasketPromotionTasks();
+        }
+
+        [Fact]
+        public void ApplyPromotionIfBasketContentsExactlyMatchPromotionRequirements()
+        {
+
+        }
+    }
+}
