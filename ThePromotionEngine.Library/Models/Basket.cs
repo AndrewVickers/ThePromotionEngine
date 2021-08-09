19d6876582614f1583c84dfdbd20a0ade2bdf518
Andrew Vickers <dev@andrewvickers.net>
2021-08-09 14:46:49 +0100

    Refactoring

--- ThePromotionEngine.Library/Models/Basket.cs
+++ ThePromotionEngine.Library/Models/Basket.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
-namespace ThePromotionEngine
+namespace ThePromotionEngine.Library.Models
 {
     public interface IBasket
     {
